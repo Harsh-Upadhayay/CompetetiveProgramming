8fafa5c9f1b090909cb767088f08629a886c6354
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-05-26 12:26:52 +0530

    autogen code

--- code.cpp
+++ code.cpp
@@ -47,7 +47,7 @@ void solve(void){
                 else if(-1 == b)
                     b = i;
                 else{
-                    cout << ar[r][a] << ar[r][b] << ar[r][i];
+                    cout << temp[a] << temp[b] << temp[i];
                     cout << "-1";
                     flag = false;
                     break;
