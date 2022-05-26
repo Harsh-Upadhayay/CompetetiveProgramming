93bc1aaa281196290f5ab729722f7671361eb831
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-05-26 13:01:42 +0530

    autogen code

--- code.cpp
+++ code.cpp
@@ -30,8 +30,9 @@ void solve(void){
         else {
             ll x1, y1, x2, y2;
             cin >> x1 >> y1 >> x2 >> y2;
-            ll rookC = *(rook_c.upper_bound(y1)), rookR = *(rook_r.upper_bound(x1));
-            
+            ll rookC = (rook_c.upper_bound(y1) == rook_c.end()?(*(rook_c.upper_bound(y1))):-1),
+             rookR = *(rook_r.upper_bound(x1));
+        
             if(
             rookC >= y1 && rookC <= y2
             || 
