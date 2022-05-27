d6e5c699103e644a483dd1bdbf9ba654201c4072
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-05-27 20:57:18 +0530

    autogen code

--- code.cpp
+++ code.cpp
@@ -10,47 +10,26 @@ using namespace std;
 #define TESTCASE
 ll t,T;
 
-ll find(ll v[], ll n, ll key){
-    ll l = 0, r = n-1;
-    while(l <= r){
-        ll mid = l + (r-l)/2;
-        if(v[mid] == key)
-            return mid;
-        else if(v[mid] < key)
-            l = mid+1;
-        else
-            r = mid-1;
-    }
-    return -1;
-}
-
 void solve(void){
-    ll n, q;
-    cin >> n >> q;
-    ll ar[n], preSum[n] = {0};
-    rpt(n)
-        cin >> ar[i];
-    sort(ar, ar+n);
-    reverse(ar, ar+n);
-    preSum[0] = ar[0];
-    for(int i = 1; i < n; i++)
-        preSum[i] = ar[i] + preSum[i-1];
-    set<ll> s;
-    reverse(ar, ar+n);
-    rpt(n)
-        s.insert(preSum[i]); 
-    rpt(q){
-        ll qry;
-        cin >> qry;
-        if(find(preSum, n, qry) != -1)
-            cout << (find(preSum, n, qry) + 1);
+    ll n,  ans;
+    cin >> n;
+    unordered_map<ll, vector<ll>> m;
+    rpt(n){
+        ll x; 
+        cin >> x;
+        auto tmp = m.find(x);
+        if(tmp != m.end())
+            (*tmp).second.pb(i);
         else{
-            auto tmp = (s.upper_bound(qry));
-            if(tmp == s.end())
-                cout << "-1";
-            else
-                cout << (find(preSum, n, *tmp) + 1);
+            vector<ll> v;
+            v.pb(i);
+            m[x] = v;
         }
+    }
+    for(auto x : m){
+        cout << x.first << " : ";
+        for(auto y : x.second)
+            cout << y << " ";
         cout << "\n";
     }
     cout<<endl;
