cf57b7f56ab91252889a1f64a319c787f393212a
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-05-27 22:08:18 +0530

    autogen code

--- code.cpp
+++ code.cpp
@@ -10,10 +10,32 @@ using namespace std;
 #define TESTCASE
 ll t,T;
 
+priority_queue<pair<ll, ll>> q;
+
+ll fun(unordered_map<ll, vector<ll>> m, ll root){
+    
+    if(!(m.count(root))){
+        q.push(make_pair(1, root));
+        return 1;
+    }
+    
+    ll ans = INT_MIN;
+    auto list = m[root];
+    for(auto x : list){
+        ll tmp = fun(m, x);
+        ans = max(ans, tmp);
+    }
+
+    q.push(make_pair(ans+1, root));
+    return ans + 1;
+}
+
 void solve(void){
+    q = priority_queue<pair<ll, ll>>();
     ll n,  ans, root;
     cin >> n;
     unordered_map<ll, vector<ll>> m;
+        
     rpt(n){
         ll x; 
         cin >> x;
@@ -30,13 +52,20 @@ void solve(void){
             m[x] = v;
         }
     }
-    for(auto x : m){
-        cout << x.first << " : ";
-        for(auto y : x.second)
-            cout << y << " ";
-        cout << "\n";
+
+
+    while(!q.empty()){
+        auto x = q.top();
+        cout << x.first << " " << x.second << " \n";
+        q.pop();
     }
-    cout<<endl;
+    // for(auto x : m){
+    //     cout << x.first << " : ";
+    //     for(auto y : x.second)
+    //         cout << y << " ";
+    //     cout << "\n";
+    // }
+    // cout<<endl;
 
 }
 
