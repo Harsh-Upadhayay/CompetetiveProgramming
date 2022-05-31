4cfb50d4866df59ba48d9ce70eee02532fe4312e
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-05-31 21:35:56 +0530

    autogen code

--- code.cpp
+++ code.cpp
@@ -36,14 +36,15 @@ void solve(void){
         }
     // cout << ans << " : ";
     for(ll i = 0; i < n-1; i++)
-        if(s[i] == '1' && s[i+1] == '1')
-            ans += 11;
-        else if(s[i] == '1' && s[i+1] == '0')
-            ans += 10;
-        else if(s[i] == '0' && s[i+1] == '1')
-            ans += 1;
-        else if(s[i] == '0' && s[i+1] == '0')
-            ans += 0;
+        cout << "HE";
+        // if(s[i] == '1' && s[i+1] == '1')
+        //     ans += 11;
+        // else if(s[i] == '1' && s[i+1] == '0')
+        //     ans += 10;
+        // else if(s[i] == '0' && s[i+1] == '1')
+        //     ans += 1;
+        // else if(s[i] == '0' && s[i+1] == '0')
+        //     ans += 0;
     cout << ans;
     cout<<endl;
 
