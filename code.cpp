a4daf4f12270115a1f9aa1bf6f5a6645b3632ab7
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-06-05 09:32:11 +0530

    autogen code

--- code.cpp
+++ code.cpp
@@ -10,52 +10,26 @@ using namespace std;
 #define TESTCAS
 ll t,T;
 
-ll  ans = 1, height = 1;
-
-void fun(map<ll, vector<ll>> &tree, ll x, ll height){
-    if(!tree.count(x)){
-        ans = max(ans, height);
-        return;
-    }
-    
-    for(auto y : tree[x])
-        fun(tree, y, height+1);
-}
-
 void solve(void){
-    ll n;
-    cin >> n;
-    map<ll, vector<ll>> tree;
-    vector<ll> roots;
-    rpt(n){
-        ll x;
-        cin >> x;
-        if(-1 == x){
-            roots.pb(i+1);
-            continue;
-        }
-        if(tree.count(x))
-            tree[x].pb(i+1);
-        else{
-            vector<ll> v;
-            v.pb(i+1);
-            tree[x] = v;
-        }
-
+    ll n, s;
+    cin >> n >> s;
+    string go, come;
+    cin >> go >> come;
+    s--;
+    if(!(go[s] || come[s])){
+        cout << "NO";
     }
-
-
-    for(auto x : roots){
-        fun(tree, x, 1);
+    else if(go[s]){
+        cout << "YES";
     }
-    cout << ans;
-
-    // for(auto x : tree){
-    //     cout << x.first << " : ";
-    //     for(auto y : x.second)
-    //         cout << y << " ";
-    //     cout << "\n";
-    // }
+    else if(come[s]){
+        bool flag = false;
+        for(int i = s+1; i < n; i++){
+            if(go[i] && come[i])
+                flag = true;
+        }
+        cout << (flag?"YES":"NO");
+    }
     cout<<endl;
 
 }
