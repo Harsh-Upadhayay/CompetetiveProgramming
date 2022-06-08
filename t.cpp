0ade325d2262f9cead246ef9db8b7c5315acf3e6
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-06-08 16:32:52 +0530

    autogen t

--- t.cpp
+++ t.cpp
@@ -54,7 +54,7 @@ int main() {
 
     #ifndef ONLINE_JUDGE
         freopen("input.txt","r",stdin);
-        // freopen("output.txt","w",stdout);
+        freopen("output.txt","w",stdout);
     #endif
 
     ios_base::sync_with_stdio(0);
