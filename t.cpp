6f5ae0f37d522a0a7064caba03a8d796cf659e5f
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-06-09 10:06:07 +0530

    autogen t

--- t.cpp
+++ t.cpp
@@ -33,6 +33,7 @@ void solve(void){
             len++;
         }
         else{
+            cout << (_r - _l);
             if(_r - _l > ans){
                 ans = _r - _l;
                 l = _l;
