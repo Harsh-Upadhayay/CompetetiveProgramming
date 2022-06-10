8d58d0706d7d8f031d9fdea541b3a9dc999a299c
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-06-10 15:18:34 +0530

    autogen t

--- t.cpp
+++ t.cpp
@@ -1,15 +1,9 @@
-#include<bits/stdc++.h>
+        #include<bits/stdc++.h>
 using namespace std;
 
 #define ll long long int
 #define pb push_back
-// #define rpt(n) for(ll i = 0; i < n; i++)
-#define rpt(i, s, e) for(ll (i) = (s); i < (e); i++)
-#define vsum(v, s, e, x) for(ll i = s; i < (e); i++) x += v[i];
-#define vin(v, s, e) for(ll i = s; i < (e); i++) cin >> (v[i]);
-#define vout(v, s, e) for(ll i = s; i < (e); i++) cout << (v[i]) << " ";
-#define vec vector<ll>
-#define nl cout << "\n";
+#define rpt(n) for(ll i = 0; i < n; i++)
 
 #define inf INT_MAX
 
@@ -17,30 +11,9 @@ using namespace std;
 ll t,T;
 
 void solve(void){
-    ll n, k;
-    cin >> n >> k;
-    vec v(n);
-    vin(v, 0, n);
-    if(n <= k){
-        ll xtra = k - n;
-        ll sum = 0;
-        vsum(v, 0, n, sum);
-        sum += (n*(n-1))/2 + n*xtra;
-        cout << sum;
-    }
-    else{
-        ll sum = 0;
-        vsum(v, 0, k, sum);
-        ll ans = sum;
-
-        rpt(j, k, n){
-            sum += v[j] - v[j-k];
-            ans = max(ans, sum);
-        }
-
-        ans += (k*(k-1))/2;
-        cout << ans;
-    }
+    ll n, m, ans;
+    cin >> n >> m;
+    cout << (!(m%2)?"YES":"NO");
     cout<<endl;
 
 }
