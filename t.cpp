04e40f15d30d276fda30c579fbe65bb93c9bb51c
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-06-10 14:17:16 +0530

    autogen t

--- t.cpp
+++ t.cpp
@@ -32,11 +32,12 @@ void solve(void){
         ll sum = 0;
         vsum(v, 0, k, sum);
         ll ans = sum;
-        cout << ans;
+
         rpt(j, k, n){
             sum += v[j] - v[j-k];
             ans = max(ans, sum);
         }
+        cout << ans;
         ans += (n*(n-1))/2;
         cout << ans;
     }
