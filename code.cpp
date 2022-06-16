264fdd9dc57f4a50b1e8e85e0e7927bf63ba58c4
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-06-17 01:26:58 +0530

    autogen code

--- code.cpp
+++ code.cpp
@@ -11,17 +11,19 @@ using namespace std;
 ll t,T;
 
 ll find(vector<ll> v, ll x){
-    ll r = v.size()-1, l = 0;
+    ll r = v.size()-1, l = 0, res = -1;
     while(l <= r){
         ll mid = r - (r-l)/2;
         if(v[mid] < x)
             l = mid + 1;
         else if(v[mid] > x)
             r = mid-1;
-        else
-            return mid;
+        else{
+            res = mid;
+            r = mid-1;
+        }
     }
-    return -1;
+    return res;
 }
 
 void solve(void){
