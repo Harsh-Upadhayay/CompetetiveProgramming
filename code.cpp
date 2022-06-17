165253088371cde1ebddfe6f5d4690044e4a2ab8
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-06-17 14:48:53 +0530

    autogen code

--- code.cpp
+++ code.cpp
@@ -29,7 +29,7 @@ void solve(void){
     for(int i = 0; i < n; i++){
         for(int j = i+1; j < n; j++)
             for(int k = j+1; k < n; k++)
-                if(v[i]+v[j]+v[k] % 3 == 0)
+                if((v[i]+v[j]+v[k]) % 3 == 0)
                     flag = true;
     }
     cout << (flag ? "YES":"NO");
