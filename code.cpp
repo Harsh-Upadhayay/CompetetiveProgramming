a1b7b3ac1b915d4127f845fbba79df34f67db48b
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-06-17 22:29:29 +0530

    autogen code

--- code.cpp
+++ code.cpp
@@ -20,7 +20,7 @@ void solve(void){
         lastIdx[v[i]] = -1;
     }
     map<ll, double> score;
-    ll curMax = INT_MIN, a, _r;
+    ll curMax = INT_MIN, a = v[0], _r = 0;
     lastIdx[v[0]] = 0;
     score[v[0]] = 2;
 
