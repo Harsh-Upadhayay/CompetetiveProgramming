7bbe7e830ee19767323ccd42cac5c3cd9624ca92
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-06-19 22:02:05 +0530

    autogen code

--- code.cpp
+++ code.cpp
@@ -10,122 +10,18 @@ using namespace std;
 #define TESTCASE
 ll t,T;
 
-
-ll _solve(ll a, ll b, ll n){
-    
-    string sa = bitset<31>(a).to_string();
-    string sb = bitset<31>(b).to_string();
-    string sn = bitset<31>(n).to_string();
-    string ans = "0000000000000000000000000000000";
-    // cout << sa[30];
-
-    for(int i = 0; i < 31; i++)
-        if(sa[i] == sb[i])
-            if(sa[i] == 0)
-                ans[i] = '1';
-            else
-                ans[i] = '0';
-        else
-            if(sa[i] != 0)
-                ans[i] = '1';
-            else
-                ans[i] = '0'; 
-
-    // cout << sa << " " << sb << ": " << ans;
-    ll _x = stoi(ans, nullptr, 2); 
-
-    bool flag = true;
-    ll count = 0;
-    if(a == b){
-        return 0;
-        // count = 0;
+void solve(void){
+    ll n, m, ans, x = 0;
+    cin >> n;
+    vector<ll> v(n);
+    rpt(n){
+        cin >> v[i];
+        x  = x ^ v[i];
     }
-    else if(_x <= n){
-        return 1;
-        // count = 1;
-    }
-    else{
-        ll maxE = 0, tn = n+1;
-        while(tn--){
-            maxE = max(maxE, _x & tn);
-        }
-        if(!maxE)
-            return -1;
-        a = a^maxE;
-        ll tpm = _solve(a, b, n); 
-        return (tpm == -1 ? -1 : 1 + tpm);
-    }
-    
-
-    // cout << (flag? count : -1);
-
+    for(auto y : v)
+        cout << (x ^ (x^y) )<< " ";
     cout<<endl;
 
-}
-ll __solve(ll a, ll b, ll n){
-    string sa = bitset<31>(a).to_string();
-    string sb = bitset<31>(b).to_string();
-    string sn = bitset<31>(n).to_string();
-    string ans = "0000000000000000000000000000000";
-    // cout << sa[30];
-
-    for(int i = 0; i < 31; i++)
-        if(sa[i] == sb[i])
-            if(sa[i] == 0)
-                ans[i] = '1';
-            else
-                ans[i] = '0';
-        else
-            if(sa[i] != 0)
-                ans[i] = '1';
-            else
-                ans[i] = '0'; 
-
-    // cout << sa << " " << sb << ": " << ans;
-    ll _x = stoi(ans, nullptr, 2); 
-    bool flag = true;
-    ll count = 0;
-    if(a == b){
-        // return 0;
-        count = 0;
-    }
-    else if(_x <= n){
-        // return 1;
-        count = 1;
-    }
-    else{
-        ll tx = log2(_x), tn = log2(n);
-        if(tn < tx)
-            count = -1;
-        else
-            count = 2;
-    }
-    return count;
-    // cout << count;
-}
-void solve(){
-    ll n, m, a, b;
-    // cin >> a >> b >> n;   
-    // ll i = a, j = b, k = n;
-    cin >> n;
-    rpt(n){
-        a = rand();
-        b = rand();
-        n = rand();
-
-        ll _ = a, j = b, k = n;
-          if(__solve(_, j, k) != _solve(_, j , k))
-            cout << _ << " " << j << " " << k << " : " << _solve(_, j, k)  << "   " << __solve(_, j, k) << endl;
-
-    }
-    // for(int i = 1; i <= a; i++)
-    //     for(int j = 1; j <= b; j++)
-    //         for(int k = 1; k <= n; k++)
-    //             if(__solve(i, j, k) != _solve(i, j , k))
-                // cout << i << " " << j << " " << k << " : " << _solve(i, j, k)  << "   " << __solve(i, j, k) << endl;
-
-    
-    cout << "\n";
 }
 
 int main() {
