043814f486ca7691187e819c85db42731ea72182
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-06-19 20:46:48 +0530

    autogen code

--- code.cpp
+++ code.cpp
@@ -41,13 +41,14 @@ void solve(void){
         for(int i = 0; i < 31; i++)
             if(ans[i] == '1'){
                 temp[i] = '1';
-                cout << temp << " " << sn << endl;
+                // cout << temp << " " << sn << endl;
                 if(temp > sn){
                     flag = false;
                     break;
                 }
             ans[i] = '0';                
             }
+            cout << ans;
     }
 
     cout << (flag? count : -1);
