903d43838fe8181aa6458e3e2ccd64931f75183c
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-06-20 23:01:31 +0530

    autogen code

--- code.cpp
+++ code.cpp
@@ -12,9 +12,9 @@ ll t,T;
 
 bool isPallindrom(ll n){
     ll rev = 0, _n = n;
-    cout << n;
+
     while(_n){
-        rev += _n % 10;
+        rev += (_n % 10)*10;
         _n /= 10;
     }
     cout << rev << " ";
