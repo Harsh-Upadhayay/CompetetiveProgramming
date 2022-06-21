f4eeb7022c1e2d3408f81078a4775b2c33ee0d9a
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-06-21 23:40:54 +0530

    autogen code

--- code.cpp
+++ code.cpp
@@ -47,7 +47,7 @@ void solve(void){
     for(int i = 1; i < n; i++)
         for(int j = 1; j < m; j++){
             rsum[i][j] = rsum[i-1][j-1];
-            lsum[i][j-1] = lsum[i-1][j+1];
+            lsum[i][j-1] = lsum[i-1][j];
         }
     
     // for(int i = 1; i < n; i++)
