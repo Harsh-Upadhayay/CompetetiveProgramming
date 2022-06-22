bb42fa6e1271a9b5a564c445d157fd9a6874c477
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-06-22 17:52:14 +0530

    autogen code

--- code.cpp
+++ code.cpp
@@ -13,7 +13,7 @@ ll t,T;
 void solve(void){
     ll n, m, ans;
     cin >> n;
-    rpt(n-1){
+    rpt((n+1)/2){
         ll x = __gcd(i+1, n-i-1);
         ans = x + (((i+1) * (n-i-1))/x);
         cout << i+1 << " " << n-i-1 << " : " << ans << "\n";
