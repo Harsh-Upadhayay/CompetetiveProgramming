b4560267a0fedd9ad9faa072af04bcde6bfa890d
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-06-25 16:33:49 +0530

    autogen code

--- code.cpp
+++ code.cpp
@@ -10,64 +10,13 @@ using namespace std;
 #define TESTCASE
 ll t,T;
 
-
-
 void solve(void){
-    ll n, xf, xs, yf, ys;
-    xf = xs = yf = ys = -1;
-    cin >> n;
-    vector<ll> v(n);
-    rpt(n)
-        cin >> v[i];
+    ll n, m, s, ans;
+    cin >> n >> s;
 
-    bool flag = false;
-
-    int i;
-    for(i = 0; i < n-1; i++)
-        if(v[i] == v[i+1]){
-            
-            if(!flag){
-                flag = true;
-                xf = i;
-                xs = i+1;
-            }
-            else
-                xs = i+1;
-        }
-        else if(flag)
-            break;
-
-    flag = false;
-
-    for(int j = n-1; j > i+1; j--)
-        if(v[j] == v[j-1]){
-            if(!flag){
-                flag = true;
-                ys = j;
-                yf = j-1;
-            }
-            else
-                yf = j-1;
-        }
-        else if(flag)
-            break;
-    ll ans = 0;
-    if(xf != -1)
-        ans = xs-xf+1;
-    if(yf != -1)
-        ans = ys-xf+1;
-    // cout << ans;
-    if(ans)
-        if(ans == 2)
-            ans = 0;
-        else if (ans == 3 || ans == 4)
-            ans = 1;
-        else if(ans == 5)
-            ans = 2;
-        else
-            ans = 3 + ans - 6;
-    cout << ans;
-    cout << "\n";
+    ll diff = s - ((n+1)*(n+2))/2;
+    cout << ceil(diff/(n*n));
+    cout<<endl;
 
 }
 
