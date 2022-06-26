f78cfda326f0a93ef7df1fe772d961d4e52a5b3c
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-06-26 16:51:22 +0530

    autogen code

--- code.cpp
+++ code.cpp
@@ -68,8 +68,8 @@ void solve(void){
 
     a = breakit(v, m);
     b = breakit(p, m);
-    // for(auto x : b)
-    //     cout << " (" << x.first << ", " << x.second << ") ";
+    for(auto x : b)
+        cout << " (" << x.first << ", " << x.second << ") ";
     
     cout << (isEqual(a, b)?"YES":"NO");
     cout<<endl;
