8e6e5e3a1c719bb596cd1f40e3eea28d5e120ae4
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-06-27 09:23:04 +0530

    autogen code

--- code.cpp
+++ code.cpp
@@ -18,8 +18,7 @@ void solve(void){
     mn = s[0];
 
     for(int i = 1; i < s.size(); i++){
-        cout << mn;
-        cout << (mn < s[i]);
+        cout << (mn > s[i]);
         if(mn < s[i]){
             mn = s[i];
             mi = i;
