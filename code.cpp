9ced9cb3ac25200a8928ac0138413cf5f9e91c02
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-06-28 18:02:57 +0530

    autogen code

--- code.cpp
+++ code.cpp
@@ -31,7 +31,7 @@ void solve(void){
         cin >> qry;
         auto l = lower_bound(preSum.begin(), preSum.end(), qry), u = upper_bound(preSum.begin(), preSum.end(), qry);
         // cout << (l == v.end()) << " ";
-        if(*l == qry)
+        if(l!= preSum.end() && *l == qry)
             cout << (l-preSum.begin()+1) << "**";
         else if(u != preSum.end())
             cout << (u-preSum.begin()+1) << "*";
