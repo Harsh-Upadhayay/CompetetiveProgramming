6fdafa4cb2704b0377a67415831c4f8fa04edbf5
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-06-28 21:13:49 +0530

    autogen code

--- code.cpp
+++ code.cpp
@@ -42,8 +42,9 @@ void solve(void){
         ans = "YES";
     flag = true;
     if(n == 4 && v.size() == 4 && sum == 0) {
-        cout << "F";
         sort(v.begin(), v.end());
+        for(auto x : v )    
+            cout << x; 
         for(int i = 0; i < 2; i++)
             if(v[i] != ((-1)*v[2+i]))
                  flag = false;
