bf55f0050853185c8fb8cb2a58899f9751053724
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-07-01 02:24:09 +0530

    autogen code

--- code.cpp
+++ code.cpp
@@ -47,9 +47,9 @@ void inline print(vector<vector<T>> v);
 
 void solve(void){
     ll a, b; cin >> a >> b;
-    debug(a, b);
+
     ll x = 0; rpt(i, 0, a) x ^ i;
-    
+    debug(x);
     ll ans = a + 1 + (x ^ a) == b;
 
     cout << ans;
