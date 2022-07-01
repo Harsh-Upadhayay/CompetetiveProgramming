3022eaf24d1bd204f034395f1949151dfd7308c8
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-07-01 18:47:49 +0530

    autogen code

--- code.cpp
+++ code.cpp
@@ -60,7 +60,7 @@ void solve(void){
     v.pb(x);
 
     vector<ll> t(all(v));
-
+    debug(v);
     rpt(i, 0, v.size()-1)
         if(v[i]%2) ans++, v[i] += (v[i] == 1 ? -1: 1), v[i+1] -= (v[i] == 1 ? -1: 1);
     cout << ans;
