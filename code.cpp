6675603dc14ece156b783930a65b36ac765f06af
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-07-01 10:48:08 +0530

    autogen code

--- code.cpp
+++ code.cpp
@@ -58,7 +58,9 @@ void solve(void){
     ll ans = 2 * (n-s.size());
     debug(s);
 
-    // for(int i = 0; i < s.size()-1; i++){
+    int i = 0;
+    cout << s.size()-1;
+    // while(i < s.size()-1){
     //     if(s[i] != s[i+1])
     //         ans += 1, s[i] = '-', s[i+1] = '-', i += 1;
     //     if(i > s.size()-1)
