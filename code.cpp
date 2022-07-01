5ffc179fe2b91597d4091afeac62f4446497e5fa
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-07-01 10:42:12 +0530

    autogen code

--- code.cpp
+++ code.cpp
@@ -60,7 +60,7 @@ void solve(void){
 
     rpt(i, 0, s.size()-1){
         if(s[i] != s[i+1])
-            ans += 1, i += 1, s[i] = '-', s[i+1] = '-';
+            ans += 1, s[i] = '-', s[i+1] = '-', i += 1;
         if(i > s.size()-1)
             break;
     }
