c316ca8f9e0771685b1ad4dcb90c4393f89179b4
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-07-03 15:19:19 +0530

    autogen code

--- code.cpp
+++ code.cpp
@@ -54,8 +54,9 @@ void solve(void){
     ll i = 0, x = 0;
     while(i < n-1){
         auto itr = s.find(v[i]);
-        while(i++ < n && *(itr++) == x) 
+        while(i++ < n && *(itr++) == x) {
             debug(x, *itr);
+        }
         x++; 
     }
     debug(x);
