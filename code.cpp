0a54f3b9a5542d49bfb3f1d575021b22cdcca5bf
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-07-06 17:16:57 +0530

    autogen code

--- code.cpp
+++ code.cpp
@@ -49,7 +49,7 @@ void inline print(vector<vector<T>> v);
 void solve(void){
     ll n, ans; cin >> n;
 
-    ll cost = ceil(((double)n)/6.0) * 15;
+    ll cost = n/6 * 15;
 
     cout << cost;    
 
