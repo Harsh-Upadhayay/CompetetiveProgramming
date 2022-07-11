3e0d167a32984d6fc35130ffb63709a5644f2ee6
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-07-12 00:25:55 +0530

    tags attrib added to problem class

--- scripts/filesCreater.py
+++ scripts/filesCreater.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import datetime
 
 class Problem:
+    
 
 
   def __init__(self, URL):
@@ -17,8 +18,14 @@ class Problem:
     
     x =  soup.find('th', attrs = {'class':"left", 'style':"width:100%;"})
     self.contest = x.text
+
+    x =  soup.findAll('span', attrs = {'class':"tag-box"})
+    self.tags = []
+    for z in x: 
+        self.tags.append(z.text.strip())
+    print(self.tags)
     
-p1 = Problem("https://codeforces.com/problemset/problem/71/A")
+p1 = Problem("https://codeforces.com/problemset/problem/266/B")
 
 print(p1.name, p1.contest)
 
