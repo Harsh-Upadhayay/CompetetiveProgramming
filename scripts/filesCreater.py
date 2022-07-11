47769f1c7710d50b541db8ba9c57384bdbe01604
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-07-12 00:59:46 +0530

    file creation automation done

--- scripts/filesCreater.py
+++ scripts/filesCreater.py
@@ -1,33 +1,15 @@
-from time import sleep
-import requests
-from bs4 import BeautifulSoup
-import datetime
+import os
+import shutil
+from Problem import Problem
+from LinkInfo import Link 
 
-class Problem:
-    
+prblm = Problem(Link().link)
+fileName = prblm.name
+folderName = prblm.contest
 
+try :
+    os.mkdir("../" + folderName)
+except : 
+    pass
 
-  def __init__(self, URL):
-    
-    URL = URL
-    r = requests.get(URL)
-    soup = BeautifulSoup(r.content, 'html5lib') 
-    
-    x =  soup.find('div', attrs = {'class':'title'}) 
-    self.name = x.text;    
-    
-    x =  soup.find('th', attrs = {'class':"left", 'style':"width:100%;"})
-    self.contest = x.text
-
-    x =  soup.findAll('span', attrs = {'class':"tag-box"})
-    self.tags = []
-    for z in x: 
-        self.tags.append(z.text.strip())
-    print(self.tags)
-    
-p1 = Problem("https://codeforces.com/problemset/problem/266/B")
-
-print(p1.name, p1.contest)
-
-
-
+shutil.copyfile("../code.cpp", "../" + folderName + "/" + fileName + ".cpp")
