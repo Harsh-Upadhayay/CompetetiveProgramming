0daa35c0737ebb650eb3b23ae0a082addb667861
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-07-12 00:27:43 +0530

    seperate file created

---
+++ scripts/Problem.py
@@ -0,0 +1,25 @@
+from time import sleep
+import requests
+from bs4 import BeautifulSoup
+import datetime
+
+class Problem:
+
+  def __init__(self, URL):
+    
+    URL = URL
+    r = requests.get(URL)
+    soup = BeautifulSoup(r.content, 'html5lib') 
+    
+    x =  soup.find('div', attrs = {'class':'title'}) 
+    self.name = x.text;    
+    
+    x =  soup.find('th', attrs = {'class':"left", 'style':"width:100%;"})
+    self.contest = x.text
+
+    x =  soup.findAll('span', attrs = {'class':"tag-box"})
+    self.tags = []
+    for z in x: 
+        self.tags.append(z.text.strip())
+    
+
