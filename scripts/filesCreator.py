8af2c2a0871f04f36663230cdead510e9ec2b9c6
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-07-12 08:35:40 +0530

    absoulte path used insteda of relative

--- scripts/filesCreator.py
+++ scripts/filesCreator.py
@@ -3,18 +3,19 @@ import shutil
 from Problem import Problem
 from LinkInfo import Link 
 from subprocess import run
+import LOCAL_VARS
 
 prblm = Problem(Link().link)
-fileName = prblm.name
-folderName = prblm.contest
+folderPath = os.path.join(LOCAL_VARS.SOURCE_DIRECTORY_PATH, prblm.contest)
+filePath = os.path.join(folderPath, prblm.name + '.cpp')
 tags = ','.join(prblm.tags)
 
 try :
-    os.mkdir("../" + folderName)
+    os.mkdir(folderPath)
 except : 
     pass
 
-shutil.copyfile("../code.cpp", "../" + folderName + "/" + fileName + ".cpp")
+shutil.copyfile(LOCAL_VARS.SOURCE_CODE_PATH, filePath)
 
-run(['git', 'add', '../' + folderName + '/' + fileName + '.cpp'])
+run(['git', 'add', filePath])
 run(['git', 'commit', '-m', tags ])
