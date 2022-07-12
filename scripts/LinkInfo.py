ee39d977e27d0bf68d1f2078841de7009f6a8753
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-07-12 08:36:10 +0530

    absolute path used instead of relative

--- scripts/LinkInfo.py
+++ scripts/LinkInfo.py
@@ -1,6 +1,7 @@
+import LOCAL_VARS
 
 class Link :
 
-    def __init__(self, file="../code.cpp"):    
+    def __init__(self, file=LOCAL_VARS.SOURCE_CODE_PATH):    
         f = open(file, "r")
         self.link = f.readline()[2 : -3]
