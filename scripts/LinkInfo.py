e441cfbddd97e3e0e6d82d7a310c31a9a8c91217
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-07-12 09:06:27 +0530

    error handling for link creation added

--- scripts/LinkInfo.py
+++ scripts/LinkInfo.py
@@ -1,7 +1,18 @@
 import LOCAL_VARS
+import sys
 
 class Link :
 
     def __init__(self, file=LOCAL_VARS.SOURCE_CODE_PATH):    
-        f = open(file, "r")
+        
+        try : 
+            f = open(file, "r")
+        except Exception as e :
+            sys.exit("\t\t!! Source File not found, check path in LOCAL_VARS !!\n", e)
+        
         self.link = f.readline()[2 : -3]
+
+        if self.link.count("codeforces.com") != 1 :
+            sys.exit("\t\t !! Problem link not added, add the link in the first line of the source code !!\n") 
+
+l = Link()
