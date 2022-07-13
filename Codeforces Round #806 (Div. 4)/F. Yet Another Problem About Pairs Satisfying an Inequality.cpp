5744d7b052a89631424a37aa0c6403668c0e29b1
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-07-13 14:10:13 +0530

    binary search,data structures,dp,greedy,sortings

---
+++ Codeforces Round #806 (Div. 4)/F. Yet Another Problem About Pairs Satisfying an Inequality.cpp
@@ -0,0 +1,142 @@
+/* https://codeforces.com/contest/1703/problem/F */
+#include<bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+    #define debug(x...) 
+#else
+    #include "debug.h"
+#endif
+
+/*_________________________________________________________________________________________________________________________________________*/
+
+#define ll                      long long int
+#define ull                     unsigned ll
+#define ld                      long double
+#define p(x, y)                 pair<x, y> 
+#define pb                      push_back
+#define fi                      first
+#define se                      second
+#define lb                      lower_bound
+#define ub                      upper_bound
+#define vll                     vector<ll>
+#define ninf                    ((ll)((-1)*1e18+5))
+#define inf                     ((ll)(1e18+5))
+#define MOD                     ((ll)(1e9+7))
+#define nmin(v)                 *min_element(all(v))
+#define nmax(v)                 *max_element(all(v))
+#define rpt(i, begin, end)      for (__typeof(end) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+#define all(x)                  (x).begin(), (x).end() 
+#define yes                     cout << "YES";
+#define no                      cout << "NO";
+#define nl                      cout << "\n";
+#define TESTCASE
+
+/*_________________________________________________________________________________________________________________________________________*/
+
+template<class T>
+T inline max(T a, T b, T c, T d = ninf, T e = ninf);
+template<class T>
+T inline min(T a, T b, T c, T d = inf, T e = inf);
+template<class T>
+void inline print(vector<T> v);
+template<class T>
+void inline print(vector<vector<T>> v);
+
+/*_________________________________________________________________________________________________________________________________________*/
+/*_________________________________________________________________________________________________________________________________________*/
+
+ll find(vector<p(ll, ll)> v, ll x) {
+
+    ll l = 0, r = v.size();
+
+    while (l < r) {
+
+        ll mid = l + (r - l) / 2;
+ 
+        if (x >= v[mid].fi)
+            l = mid + 1;
+        else
+            r = mid;
+    }
+   
+    if(l < v.size() && v[l].fi <= x) {
+       l++;
+    }
+ 
+    return l;
+}
+
+void solve(void){
+    ll n, ans = 0; cin >> n;
+    vll v(n+1); vector<p(ll, ll)> pairs; rpt(i, 1, n+1) {cin >> v[i]; if(v[i] < i) pairs.pb({v[i], i});}
+    sort(all(pairs));
+    debug(pairs);
+
+    for(auto x : pairs){
+        ans += pairs.size() - find(pairs, x.se);
+        // cout << ans;
+    }
+    
+    cout << ans;
+
+    nl;
+}
+
+
+/*_________________________________________________________________________________________________________________________________________*/
+/*_________________________________________________________________________________________________________________________________________*/
+
+int main() {
+    srand(time(0));
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+        freopen("err.txt","w",stderr);
+    #endif
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    
+    ll t = 1;
+
+    #ifdef TESTCASE
+        cin >> t;
+    #endif
+
+    while(t--) 
+        solve();
+
+    return 0;
+}
+
+/*_________________________________________________________________________________________________________________________________________*/
+
+template<class T>
+T inline max(T a, T b, T c, T d, T e){
+    return max(max(max(a, b), max(c, d)), e);
+}
+
+template<class T>
+T inline min(T a, T b, T c, T d, T e){
+    return min(min(min(a, b), min(c, d)), e);
+}
+
+template<class T>
+void inline print(vector<T> v){
+    for(auto x : v)
+        cout << x << " ";
+    nl;
+}
+
+template<class T>
+void inline print(vector<vector<T>> v){
+    for(auto x : v){
+        for(auto y : x)
+            cout << y << " ";
+        nl;
+    }
+}
+
+/*_________________________________________________________________________________________________________________________________________*/
