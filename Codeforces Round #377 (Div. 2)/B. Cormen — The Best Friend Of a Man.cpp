21d159760c7bcedf9000b2b6d6d3941396d68770
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-07-19 13:57:51 +0530

    dp,greedy,*1000

--- Codeforces Round #377 (Div. 2)/B. Cormen — The Best Friend Of a Man.cpp
+++ Codeforces Round #377 (Div. 2)/B. Cormen — The Best Friend Of a Man.cpp
@@ -63,11 +63,9 @@ void solve(void){
     vll v(n); rpt(i, 0, n) cin >> v[i];
 
     ll ans = 0;
-    for(int i = 1; i < n - 1; i++){
-            debug(v[i], v[i-1], ans);
+    for(int i = 1; i < n; i++){
         if(v[i] + v[i-1] < k) {
             ans += k - (v[i] + v[i-1]);
-            debug(k, v[i], v[i-1]);
             v[i] += k - (v[i] + v[i-1]);
         }
     }
