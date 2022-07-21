fc9243b0e50f7a49b62e782bb28e451db87e988d
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-07-21 23:11:48 +0530

    data structures,greedy,*1100

--- Codeforces Round #805 (Div. 3)/C. Train and Queries.cpp
+++ Codeforces Round #805 (Div. 3)/C. Train and Queries.cpp
@@ -57,7 +57,7 @@ void init(){
     return;
 }
 
-void seolve()
+void solve()
 {
     ll N,M;
     cin>>N>>M;
@@ -108,7 +108,7 @@ void seolve()
 
 }
 
-void solve(void){
+void seolve(void){
     ll n, m; cin >> n >> m;
     vll v(n + 1); rpt(i, 1, n + 1) cin >> v[i];
     vll tmp(n + 1);
