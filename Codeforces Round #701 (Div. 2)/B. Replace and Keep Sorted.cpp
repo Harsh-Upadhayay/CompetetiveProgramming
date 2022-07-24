a8e64f4d71ccd6001203a8ee79685015ffd32f5e
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-07-24 10:57:04 +0530

    dp,implementation,math,*1200

--- Codeforces Round #701 (Div. 2)/B. Replace and Keep Sorted.cpp
+++ Codeforces Round #701 (Div. 2)/B. Replace and Keep Sorted.cpp
@@ -82,7 +82,7 @@ void solve(void){
     while(q--) {
         ll l , r; cin >> l >> r;
         if(l == r) {
-            cout << k - 1;
+            cout << k - 1; nl;
             continue;
         }
         ll ans = (v[l] - 1) + (v[l + 1] - v[l] - 1) + (k - v[r]) + (v[r] - v[r - 1] - 1);
