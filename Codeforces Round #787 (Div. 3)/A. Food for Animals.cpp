6c9999fdb13950bda4c86f26a5ccc36b5da7674e
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-07-26 22:24:48 +0530

    greedy,math,*800

--- Codeforces Round #787 (Div. 3)/A. Food for Animals.cpp
+++ Codeforces Round #787 (Div. 3)/A. Food for Animals.cpp
@@ -61,7 +61,6 @@ void init(){
 void solve(void){
     ll a, b, c, x, y; cin >> a >> b >> c >> x >> y;
     x -= a, y -= b;
-    debug(x, y);
     ll rem = max(x, 0ll) + max(y, 0ll);
     debug(rem);
     cout << (rem >= c ? "YES":"NO");
