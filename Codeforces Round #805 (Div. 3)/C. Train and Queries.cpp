b21c20e843c72e7df06031b848c7e476adea08b0
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-07-30 11:45:26 +0530

    data structures,greedy,*1100

--- Codeforces Round #805 (Div. 3)/C. Train and Queries.cpp
+++ Codeforces Round #805 (Div. 3)/C. Train and Queries.cpp
@@ -57,40 +57,70 @@ void init(){
     return;
 }
 
-
+class Solution {
+    
+    map<pair<int, int>, list<pair<int, int>>> graph;
+    set<pair<int, int>> visited;
+    
+    void addEdge(int i, int j, int u, int v) {
+        graph[{i, j}].push_back({u, v});
+        if(u != -1 && v != -1)
+            graph[{u, v}].push_back({i, j});
+    }
+    
+    void build(vector<vector<char>> &grid) {
+        
+        graph.clear();
+        visited.clear();
+        int n = grid.size(), m = grid[0].size();
+        map<pair<int, int>, list<pair<int, int>>> graph;
+        
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++) 
+                if(grid[i][j] == '1') {
+                    bool isolated = true;
+                    if(i != 0     && grid[i - 1][j] == '1') addEdge(i, j, i - 1, j), isolated = false;
+                    if(i != n - 1 && grid[i + 1][j] == '1') addEdge(i, j, i + 1, j), isolated = false;
+                    if(j != n - 1 && grid[i][j + 1] == '1') addEdge(i, j, i, j + 1), isolated = false;
+                    if(j != 0     && grid[i][j - 1] == '1') addEdge(i, j, i, j - 1), isolated = false;
+                    if(isolated) addEdge(i, j, -1, -1);
+                }       
+    }
+    
+    void dfs(pair<int, int> source) {
+        if(visited.count(source))
+            return;
+        visited.insert(source);
+        
+        for(auto node : graph[source])
+            dfs(node);
+    }
+    
+public:
+    
+    int numIslands(vector<vector<char>>& grid) {
+        
+        build(grid);
+        
+        int islands = 0;
+        for(auto node : graph) 
+            if(!visited.count(node.first))
+                dfs(node.first), islands++;
+        
+        return islands;
+    }
+};
 
 void solve(void){
-    ll n; cin >> n;
-    vll v(n); rpt(i, 0, n) cin >> v[i];
-    sort(all(v), greater<ll>());
-    if(n == 1) {
-        cout << "0\n"; return;
-    }
-    bool flag = true;
-    ll x = v[0];
-    rpt(i, 0, n) flag = flag & v[i] == x;
-    if(flag) {cout << "0\n"; return;}
+    
+    int n, m; cin >> n >> m;
+    vector<vector<int>> grid(n, vector<int> (m));
+    vector<vector<char>> v(n, vector<char> (m));
 
-    ll sum = 0; for(auto x : v) sum += x;
-
-    vll possible(n);
-
-    rpt(i, 0, n)
-        possible[i] = v[i] * (n - i);
-    debug(possible);
-
-    ll curr = v[0], ans = 1;
-    sum -= v[0];
-    rpt(i, 1, n) {
-        if(curr >= possible[i] - sum)
-            break;
-        ans++;
-        curr += v[i];
-        sum -= v[i];
-    }
-    cout << ans;
-
-    // cout << ans;
+    rpt(i, 0, n) rpt(j, 0, m) cin >> grid[i][j];
+    rpt(i, 0, n) rpt(j, 0, m) v[i][j] = (grid[i][j] ? '1' : '0');
+    Solution obj;
+    obj.numIslands(v);
 
     nl;
 }
