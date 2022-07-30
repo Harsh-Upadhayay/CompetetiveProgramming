a85607762931e100fd7acc7e15d4bfdf016603ee
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-07-30 12:32:29 +0530

    Time: 63 ms (39.14%), Space: 12.3 MB (55.36%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,53 +1,28 @@
 class Solution {
     
-    map<pair<int, int>, list<pair<int, int>>> graph;
-    set<pair<int, int>> visited;
-    
-    void addEdge(int i, int j, int u, int v) {
-        graph[{i, j}].push_back({u, v});
-        if(u != -1 && v != -1)
-            graph[{u, v}].push_back({i, j});
-    }
-    
-    void build(vector<vector<char>> &grid) {
-        
-        graph.clear();
-        visited.clear();
-        int n = grid.size(), m = grid[0].size();
-        map<pair<int, int>, list<pair<int, int>>> graph;
-        
-        for(int i = 0; i < n; i++)
-            for(int j = 0; j < m; j++) 
-                if(grid[i][j] == '1') {
-                    bool isolated = true;
-                    if(i != 0     && grid[i - 1][j] == '1') addEdge(i, j, i - 1, j), isolated = false;
-                    if(i != n - 1 && grid[i + 1][j] == '1') addEdge(i, j, i + 1, j), isolated = false;
-                    if(j != m - 1 && grid[i][j + 1] == '1') addEdge(i, j, i, j + 1), isolated = false;
-                    if(j != 0     && grid[i][j - 1] == '1') addEdge(i, j, i, j - 1), isolated = false;
-                    if(isolated) addEdge(i, j, -1, -1);
-                }       
-    }
-    
-    void dfs(pair<int, int> source) {
-        if(visited.count(source))
+    void dfs(vector<vector<char>> &grid, int i, int j) {
+        if( i < 0                   ||
+            j < 0                   ||
+            i >= grid   .size()     ||
+            j >= grid[0].size()     ||
+            grid[i][j] == '0'       ||
+            grid[i][j] == '2')
             return;
-        visited.insert(source);
-        
-        for(auto node : graph[source])
-            dfs(node);
+        grid[i][j] = '2';
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i, j + 1);
+        dfs(grid, i + 1, j);
     }
+
     
 public:
-    
     int numIslands(vector<vector<char>>& grid) {
-        
-        build(grid);
-        
         int islands = 0;
-        for(auto node : graph) 
-            if(!visited.count(node.first))
-                dfs(node.first), islands++;
-        
+        for(int i = 0; i < grid.size(); i++)
+            for(int j = 0; j < grid[0].size(); j++)
+                if(grid[i][j] == '1')
+                    dfs(grid, i, j), islands++;
         return islands;
     }
 };
