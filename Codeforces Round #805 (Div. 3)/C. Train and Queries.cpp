c8ee9baf101f496ea6fb057126a1931b38959eb8
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-07-31 13:38:55 +0530

    data structures,greedy,*1100

--- Codeforces Round #805 (Div. 3)/C. Train and Queries.cpp
+++ Codeforces Round #805 (Div. 3)/C. Train and Queries.cpp
@@ -64,9 +64,11 @@ int sumOddLengthSubarrays(vector<int>& arr) {
     for(int _ = 0; _ < arr.size() / 2; _++) {
         int i = _ + 1;
         if(i % 2) {
+            sum += arr[_] * (((i * (i + 1) / 2) - (i / 2)) + ((n + 1) / 2 - i) * i) + arr[n - i - 1] * (((i * (i + 1) / 2) - (i / 2)) + ((n + 1) / 2 - i) * i);
             debug(i, (((i * (i + 1) / 2) - (i / 2)) + ((n + 1) / 2 - i) * i));
         }
         else {
+            sum += arr[_] * ( i * i / 2 + ((n + 1) / 2 - i) * i)  + arr[n - i - 1] * ( i * i / 2 + ((n + 1) / 2 - i) * i) ;
             debug(i, ( i * i / 2 + ((n + 1) / 2 - i) * i) );
         }
     }
