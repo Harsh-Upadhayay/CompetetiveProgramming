996a36436e5e6ecc00f18ef8ffd20be2af3b26c3
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-07-31 14:54:27 +0530

    data structures,greedy,*1100

--- Codeforces Round #805 (Div. 3)/C. Train and Queries.cpp
+++ Codeforces Round #805 (Div. 3)/C. Train and Queries.cpp
@@ -67,14 +67,14 @@ int sumOddLengthSubarrays(vector<int>& arr) {
             if(i % 2) {
                 sum += (arr[_] * (((i * (i + 1) / 2) - (i / 2)) + ((n + 1) / 2 - i) * i)) +
                  (arr[n - _ - 1] * (((i * (i + 1) / 2) - (i / 2)) + ((n + 1) / 2 - i) * i));
-                debug(i, (((i * (i + 1) / 2) - (i / 2)) + ((n + 1) / 2 - i) * i));
-                 debug(sum);
+                // debug(i, (((i * (i + 1) / 2) - (i / 2)) + ((n + 1) / 2 - i) * i));
+                //  debug(sum);
             }
             else {
                 sum += (arr[_] * ( i * i / 2 + ((n + 1) / 2 - i) * i))  +
                 (arr[n - _ - 1] * ( i * i / 2 + ((n + 1) / 2 - i) * i) );
-                debug(i, ( i * i / 2 + ((n + 1) / 2 - i) * i) );
-                debug(sum);
+                // debug(i, ( i * i / 2 + ((n + 1) / 2 - i) * i) );
+                // debug(sum);
             }
         }
         int i = (n+1) /2;
@@ -85,10 +85,12 @@ int sumOddLengthSubarrays(vector<int>& arr) {
         for(int _ = 0; _ < n / 2; _++) {
             int i = _ + 1;
             if(i % 2) {
+                debug((i * (i + 1) / 2 + (n / 2 - i) * i));
                 sum += (arr[_] * (i * (i + 1) / 2 + (n / 2 - i) * i)) + 
                         (arr[n - _ - 1] * (i * (i + 1) / 2 + (n / 2 - i) * i));
             }
             else {
+                debug((((i * (i + 2)/ 2) - 2) + (n / 2 - i) * i));
                 sum += (arr[_] * (((i * (i + 2)/ 2) - 2) + (n / 2 - i) * i)) + 
                         (arr[n - _ - 1] * (((i * (i + 2)/ 2) - 2) + (n / 2 - i) * i));
             }
