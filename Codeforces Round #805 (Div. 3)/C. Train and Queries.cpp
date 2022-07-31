67eb310bd295646f607b394753576905ff90d280
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-07-31 21:41:09 +0530

    data structures,greedy,*1100

--- Codeforces Round #805 (Div. 3)/C. Train and Queries.cpp
+++ Codeforces Round #805 (Div. 3)/C. Train and Queries.cpp
@@ -64,8 +64,8 @@ void solve(void){
 
     ll i = 0, ans = 0;
     while(i < n) {
-        ll maxDiff = ninf, mine = v[i], maxe = v[i];;
-        while(maxDiff <= 2 * x && i < n) {
+        ll maxDiff = ninf, mine = v[i], maxe = v[i], itr = 5;
+        while(maxDiff <= 2 * x && i < n - 1 && itr--) {
             i++;
             maxDiff = max(maxDiff, abs(mine - maxe));
             mine = min(mine, v[i]);
