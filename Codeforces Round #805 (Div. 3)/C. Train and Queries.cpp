06403c2d3612f7a74f46f7c32d852f9ae0f67ad6
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-07-31 20:56:42 +0530

    data structures,greedy,*1100

--- Codeforces Round #805 (Div. 3)/C. Train and Queries.cpp
+++ Codeforces Round #805 (Div. 3)/C. Train and Queries.cpp
@@ -75,8 +75,8 @@ void solve(void){
             maxNE = v[i]; maxN = abs(v[i] - a); maxPE = v[i - 1]; maxP = abs(v[i-1] - a);
         }
 
-                debug(a);
-        if(abs(a - v[i] <= x)) {
+        debug(a);
+        if(abs(a - v[i]) <= x) {
             while(i < n - 1) {
                 i++;
                 if(abs(a - v[i]) <= x) {
