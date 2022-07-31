9e20d41dbf92f588dd2bf5889553d20762d99c69
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-07-31 19:45:00 +0530

    data structures,greedy,*1100

--- Codeforces Round #805 (Div. 3)/C. Train and Queries.cpp
+++ Codeforces Round #805 (Div. 3)/C. Train and Queries.cpp
@@ -64,9 +64,9 @@ void solve(void){
 
     if(m > n) {   cout << "NO"; nl; return;    }
 
-    rpt(i, n, m - 1) {
+    rpt(i, 0, m - 1) {
         debug(i, a[i], b[i]);
-        if(a[i] != b[i]) {   cout << "NsO"; nl; return;    }
+        if(a[n - i - 1] != b[m - i - 1]) {   cout << "NsO"; nl; return;    }
     }
 
     rpt(i, 0, n - m) {
