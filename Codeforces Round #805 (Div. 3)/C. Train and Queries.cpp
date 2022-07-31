834a7f6d641daae6af10c4a70729833dc4e51223
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-07-31 14:50:14 +0530

    data structures,greedy,*1100

--- Codeforces Round #805 (Div. 3)/C. Train and Queries.cpp
+++ Codeforces Round #805 (Div. 3)/C. Train and Queries.cpp
@@ -61,24 +61,39 @@ int sumOddLengthSubarrays(vector<int>& arr) {
     if(!(arr.size()/ 2))
         return -1;
     int sum = 0, n = arr.size();
-    for(int _ = 0; _ < arr.size() / 2; _++) {
-        int i = _ + 1;
-        if(i % 2) {
-            sum += (arr[_] * (((i * (i + 1) / 2) - (i / 2)) + ((n + 1) / 2 - i) * i)) +
-             (arr[n - _ - 1] * (((i * (i + 1) / 2) - (i / 2)) + ((n + 1) / 2 - i) * i));
-            debug(i, (((i * (i + 1) / 2) - (i / 2)) + ((n + 1) / 2 - i) * i));
-             debug(sum);
+    if(n % 2) {
+        for(int _ = 0; _ < arr.size() / 2; _++) {
+            int i = _ + 1;
+            if(i % 2) {
+                sum += (arr[_] * (((i * (i + 1) / 2) - (i / 2)) + ((n + 1) / 2 - i) * i)) +
+                 (arr[n - _ - 1] * (((i * (i + 1) / 2) - (i / 2)) + ((n + 1) / 2 - i) * i));
+                debug(i, (((i * (i + 1) / 2) - (i / 2)) + ((n + 1) / 2 - i) * i));
+                 debug(sum);
+            }
+            else {
+                sum += (arr[_] * ( i * i / 2 + ((n + 1) / 2 - i) * i))  +
+                (arr[n - _ - 1] * ( i * i / 2 + ((n + 1) / 2 - i) * i) );
+                debug(i, ( i * i / 2 + ((n + 1) / 2 - i) * i) );
+                debug(sum);
+            }
         }
-        else {
-            sum += (arr[_] * ( i * i / 2 + ((n + 1) / 2 - i) * i))  +
-            (arr[n - _ - 1] * ( i * i / 2 + ((n + 1) / 2 - i) * i) );
-            debug(i, ( i * i / 2 + ((n + 1) / 2 - i) * i) );
-            debug(sum);
+        int i = (n+1) /2;
+
+        sum += arr[i - 1] * (i % 2 ? ((i * (i + 1) / 2) - (i / 2)) : (i * i) / 2 );
+    }
+    else {
+        for(int _ = 0; _ < n / 2; _++) {
+            int i = _ + 1;
+            if(i % 2) {
+                sum += (arr[_] * (i * (i + 1) / 2 + (n / 2 - i) * i)) + 
+                        (arr[n - _ - 1] * (i * (i + 1) / 2 + (n / 2 - i) * i));
+            }
+            else {
+                sum += (arr[_] * (((i * (i + 2)/ 2) - 2) + (n / 2 - i) * i)) + 
+                        (arr[n - _ - 1] * ((i * (i + 2)/ 2) - 2) + (n / 2 - i) * i);
+            }
         }
     }
-    int i = (n+1) /2;
-
-    sum += arr[i - 1] * (i % 2 ? ((i * (i + 1) / 2) - (i / 2)) : (i * i) / 2 );
     // debug((i % 2 ? ((i * (i + 1) / 2) - (i / 2)) : (i * i) / 2 ));
     return sum;
 }
