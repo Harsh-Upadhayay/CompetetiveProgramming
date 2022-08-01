1541e5012ef4ca73612b59b7500bb12465d16f58
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-08-01 12:51:39 +0530

    greedy,sortings

--- CodeTON Round 2 (Div. 1 + Div. 2, Rated, Prizes!)/C. Virus.cpp
+++ CodeTON Round 2 (Div. 1 + Div. 2, Rated, Prizes!)/C. Virus.cpp
@@ -61,7 +61,9 @@ void init(){
 void solve(void){
     ll n, m; cin >> n >> m;
     vll v(m); rpt(i, 0, m) cin >> v[i];
-    
+    if(n == m) {
+        cout << "0\n"; return;
+    }
     vll diff(m); rpt(i, 0, m - 1) diff[i] = max(ninf, abs(v[i] - v[i + 1]) - 1);
     diff[m - 1] = v[0] + n - v[m - 1] - 1;
     debug(diff);
