6a1d84b8e5db54cadb83692d381e0858a2172183
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-08-01 12:41:57 +0530

    greedy,sortings

--- CodeTON Round 2 (Div. 1 + Div. 2, Rated, Prizes!)/C. Virus.cpp
+++ CodeTON Round 2 (Div. 1 + Div. 2, Rated, Prizes!)/C. Virus.cpp
@@ -77,11 +77,10 @@ void solve(void){
         }
         else break;
     }
-    debug(diff);
     ll ans = 0;
     for(auto x : diff)
         if(x > 0) ans += x;
-    debug(ans);
+    print(diff);
     cout << n - ans;
 
     nl;
