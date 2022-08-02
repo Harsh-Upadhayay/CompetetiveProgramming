9f2bd08a31043fef36f5290f0aed0860b2bff7bf
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-08-02 12:20:08 +0530

    Time: 202 ms (87.15%), Space: 65.2 MB (49.05%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.cpp
+++ 997-find-the-town-judge/997-find-the-town-judge.cpp
@@ -2,17 +2,16 @@ class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
         vector<int> graph[n + 1];
-        for(auto x : trust)
-            graph[x[0]].push_back(x[1]);
+        for(int i = 0; i < trust.size(); i++)
+            graph[trust[i][0]].push_back(trust[i][1]);
         vector<int> indegree(n + 1, 0);
         
         for(int i = 1; i <= n; i++)
-            for(auto x : graph[i])
-                indegree[x]++;
+            for(int j = 0; j < graph[i].size(); j++)
+                indegree[graph[i][j]]++;
         
         int ans = -1;
         for(int i = 1; i <= n; i++)
-            // cout << indegree[i] << " ";
             if(indegree[i] == n -1 && graph[i].size() == 0)
                 ans = i;
         return ans;
