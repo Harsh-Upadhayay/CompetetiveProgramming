688a77edd4be78030b526e0652dbe7f089840189
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-08-02 12:20:52 +0530

    Time: 358 ms (23.00%), Space: 65.3 MB (49.05%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.cpp
+++ 997-find-the-town-judge/997-find-the-town-judge.cpp
@@ -4,8 +4,8 @@ public:
         vector<int> graph[n + 1];
         for(int i = 0; i < trust.size(); i++)
             graph[trust[i][0]].push_back(trust[i][1]);
-        vector<int> indegree(n + 1, 0);
         
+        vector<int> indegree(n + 1, 0);
         for(int i = 1; i <= n; i++)
             for(int j = 0; j < graph[i].size(); j++)
                 indegree[graph[i][j]]++;
