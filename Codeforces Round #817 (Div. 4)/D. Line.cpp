f2dedec8355b78f1367f32ef7118a0ec48f93fe8
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-08-31 10:31:06 +0530

    greedy,sortings

--- Codeforces Round #817 (Div. 4)/D. Line.cpp
+++ Codeforces Round #817 (Div. 4)/D. Line.cpp
@@ -71,12 +71,12 @@ void solve(void){
 
         if(init[l] == 'L') {
             while(init[l] == 'R' && l < n / 2) l++; 
-            if(init[l] == 'L') {init[l] = 'R'; score += (n - 2 * l - 1);}
+            if(init[l] == 'L') {init[l] = 'R'; score += (n - 2 * l - 1); l++;}
         }
 
         else if(init[r] == 'R') {
             while(init[r] == 'l' && r > n / 2) r--;
-            if(init[r] == 'R') {init[r] = 'L'; score += (2* r - n + 1);}
+            if(init[r] == 'R') {init[r] = 'L'; score += (2* r - n + 1); r--;}
         }
         debug(init);
         cout << score << " ";
