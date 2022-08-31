d610db2d43767abb48ce4ac61d63056ec175e006
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-08-31 09:50:34 +0530

    implementation

--- Codeforces Round #817 (Div. 4)/A. Spell Check.cpp
+++ Codeforces Round #817 (Div. 4)/A. Spell Check.cpp
@@ -64,6 +64,7 @@ void solve(void){
     string s; cin >> s;
     string name = "Timur";
     sort(s.begin(), s.end());
+    cout << name[0];
     cout << s;
     cout << (s == name ? "YES":"NO");
     nl;
