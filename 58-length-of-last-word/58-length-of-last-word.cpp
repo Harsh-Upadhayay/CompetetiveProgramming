784dea3d72cb99c9627e17943d2158e963d92028
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-08-31 16:36:33 +0530

    Time: 4 ms (39.47%), Space: 6.5 MB (88.88%) - LeetHub

---
+++ 58-length-of-last-word/58-length-of-last-word.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i = s.length() - 1; while(i >= 0 && s[i] == ' ') i--;
+        int j = i; for(; j >= 0 && s[j] != ' '; j--);
+        return i - j;
+    }
+};
