6978fcf5fc7ac36910203a7fe2da96be7615ddd0
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-09-11 10:03:52 +0530

    data structures,greedy,*1100

--- Codeforces Round #805 (Div. 3)/C. Train and Queries.cpp
+++ Codeforces Round #805 (Div. 3)/C. Train and Queries.cpp
@@ -116,7 +116,7 @@ T inline min(T a, T b, T c, T d, T e){
 template<class T>
 void inline print(vector<T> v){
     for(auto x : v)
-        cout << x << " ";
+        cout << x << " \n";
 }
 
 template<class T>
