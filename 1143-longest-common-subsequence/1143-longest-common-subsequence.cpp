9cce5101a79697cd80885dbfafd6b8bf98fa3271
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-10-08 16:27:04 +0530

    Time: 51 ms (35.12%), Space: 13.1 MB (24.79%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -7,12 +7,9 @@ class Solution {
         for(int i = 0; i <= n; i++) dp[i][0] = 0;
         for(int j = 0; j <= m; j++) dp[0][j] = 0;
 
-        s = ' ' + s;
-        t = ' ' + t;
-
         for(int i = 1; i <= n; i++) 
             for(int j = 1; j <= m; j++) 
-                if(s[i] == t[j])
+                if(s[i - 1] == t[j - 1])
                     dp[i][j] = 1 + dp[i - 1][j - 1];
                 else
                     dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);   
