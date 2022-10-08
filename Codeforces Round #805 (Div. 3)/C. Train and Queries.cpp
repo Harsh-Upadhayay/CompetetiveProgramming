35752be3d37fa9001f86af0c3ce537e8264e4311
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-10-08 09:45:29 +0530

    data structures,greedy,*1100

--- Codeforces Round #805 (Div. 3)/C. Train and Queries.cpp
+++ Codeforces Round #805 (Div. 3)/C. Train and Queries.cpp
@@ -66,7 +66,7 @@ void solve(void){
     ll ls = ceil(sqrt(l));
     ll rs = floor(sqrt(r));
 
-    debug(ls, rs, (rs - ls) * 3);
+    debug(ls, rs);
     ll ans = 3 * (rs - ls) + 1;    
     ls -= 1;
 
