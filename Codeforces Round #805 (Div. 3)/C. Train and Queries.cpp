fde92e9b5d4731f9326863d9c4772c90c46d9391
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-10-09 14:55:22 +0530

    data structures,greedy,*1100

--- Codeforces Round #805 (Div. 3)/C. Train and Queries.cpp
+++ Codeforces Round #805 (Div. 3)/C. Train and Queries.cpp
@@ -89,7 +89,7 @@ void solve(void){
     cout << "(" << n << ", " << m << ")" << " : " << f(n, m, 0xFFFFFFFF, v) << "\n";
 
     for(auto x : freq)
-        cout <<  x.first << " : " << x.second << ", " << setw(80) << bitset<64>(x.second) << "\n";
+        cout <<  x.first << " : " << x.second << ", " << bitset<64>(x.second) << "\n";
 
     nl;
     nl;
