cc8c15b1356cb2911aac1e83b39988bc6500eecb
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-10-12 14:36:39 +0530

    Time: 1633 ms (8.32%), Space: 50.5 MB (12.50%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -10,14 +10,25 @@ class Solution {
             if(ans) break;
             ans |= f(nums, dp, i + j);
         }
-            
         
         return dp[i] = ans;
     }
     
 public:
     bool canJump(vector<int>& nums) {
-        vector<int> dp(nums.size(), -1);
-        return f(nums, dp, 0);
+        int n = nums.size();
+        vector<int> dp(n, -1);
+        
+        dp[n - 1] = 1;
+        for(int i =  n - 2; i >= 0; i--) {
+            bool ans = false;
+            for(int j = 1; j <= nums[i]; j++) {
+                if(ans) break;
+                ans |= dp[i + j];
+            }
+
+            dp[i] = ans;
+        }
+        return dp[0];
     }
 };
