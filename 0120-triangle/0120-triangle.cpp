4e7039a5ab00dfce705d75fad488a1c8bd08bb3e
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-10-22 11:24:55 +0530

    Time: 8 ms (79.26%), Space: 8.5 MB (93.47%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,19 +1,4 @@
-#define inf 1e8
 class Solution {
-    
-    int f(vector<vector<int>>& arr, vector<vector<int>> &dp, int i, int j) {
-        
-        if(i == arr.size()) return 0;
-        if(j == arr[i].size()) return inf;
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        int left = 0, right = 0;
-        left = arr[i][j] + f(arr, dp, i + 1, j);
-        right = arr[i][j] + f(arr, dp, i + 1, j + 1);
-        
-        return dp[i][j] = min(left, right);
-    }
-    
 public:
     int minimumTotal(vector<vector<int>>& arr) {
         
