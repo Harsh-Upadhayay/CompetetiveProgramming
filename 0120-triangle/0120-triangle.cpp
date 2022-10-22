949c05b5b83a6217a12da4b7cbb6ed6aaf79c4a3
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-10-22 11:24:38 +0530

    Time: 7 ms (86.90%), Space: 8.6 MB (78.28%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -20,16 +20,9 @@ public:
         int n = arr.size();
         vector<int> cur(n + 1, 0), nxt(cur);
         
-        for(int i = n - 1; i >= 0; i--, nxt = cur) {
-            for(int j = i; j >= 0; j--) {
-                
-                int left = 0, right = 0;
-                left = arr[i][j] + nxt[j];
-                right = arr[i][j] + nxt[j + 1];
-
-                cur[j] = min(left, right);
-            }
-        }
+        for(int i = n - 1; i >= 0; i--, nxt = cur) 
+            for(int j = i; j >= 0; j--) 
+                cur[j] = min(arr[i][j] + nxt[j], arr[i][j] + nxt[j + 1]);
         
         return cur[0];
     }
