b9a5c92ef91dea8657798124a0f14b58c17cc02e
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-10-22 11:14:58 +0530

    Time: 14 ms (41.31%), Space: 9.1 MB (9.84%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,25 @@
+#define inf 1e8
+class Solution {
+    
+    int f(vector<vector<int>>& arr, vector<vector<int>> &dp, int i, int j) {
+        
+        if(i >= arr.size()) return 0;
+        if(j >= arr[i].size()) return inf;
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        int left = 0, right = 0;
+        left = arr[i][j] + f(arr, dp, i + 1, j);
+        right = arr[i][j] + f(arr, dp, i + 1, j + 1);
+        
+        return dp[i][j] = min(left, right);
+    }
+    
+public:
+    int minimumTotal(vector<vector<int>>& arr) {
+        
+        int n = arr.size();
+        vector<vector<int>> dp(n + 1, vector<int> (n + 1, -1));
+        
+        return f(arr, dp, 0, 0);
+    }
+};
