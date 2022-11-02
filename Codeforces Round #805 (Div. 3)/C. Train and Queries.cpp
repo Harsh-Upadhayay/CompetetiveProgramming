cde737cd5921fd9aebcf0814cc8dbee7985e672f
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-11-02 21:07:29 +0530

    data structures,greedy,*1100

--- Codeforces Round #805 (Div. 3)/C. Train and Queries.cpp
+++ Codeforces Round #805 (Div. 3)/C. Train and Queries.cpp
@@ -24,7 +24,7 @@ long long f(vector<vector<long long>> &grid, vector<vector<long long>> &dp, int 
     for(int itr_i = i - 1; itr_i >= 0; itr_i--)
         left  = grid[itr_i][j] * (left % MOD + f(grid, dp, itr_i, j) % MOD) % MOD;
 
-    // cout << up << " " << left << " " << diag << "\n";
+    cout << up << " " << left << " " << diag << "\n";
 
     return dp[i][j] = (up + left + diag);
 }
