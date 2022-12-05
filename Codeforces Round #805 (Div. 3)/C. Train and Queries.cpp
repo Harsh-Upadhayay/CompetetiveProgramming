290b92a06816c42bd8de8d7b7f3f1a8c6cd1a56f
Harsh Upadhayay <harshupadhayay906@gmail.com>
2022-12-05 18:06:02 +0530

    data structures,greedy,*1100

--- Codeforces Round #805 (Div. 3)/C. Train and Queries.cpp
+++ Codeforces Round #805 (Div. 3)/C. Train and Queries.cpp
@@ -1,48 +1,152 @@
+/*  */
 #include<bits/stdc++.h>
 using namespace std;
 
-#define MOD ((long long)1000000007)
+#ifdef DarkLord
+    #include "debug.h"
+#else
+    #define debug(x...) 
+    class Timer{};
+#endif
 
-long long f(vector<vector<long long>> &grid, vector<vector<long long>> &dp, int i, int j) {
-    // cout << i << " " << j << "\n";
-    if(i < 0 || j < 0) return 0;
-    if(i == 0 && j == 0) return 1;
-    if(dp[i][j] != -1) return dp[i][j];
-    // diag
+/*_________________________________________________________________________________________________________________________________________*/
 
-    long long diag = 0;
-    for(int itr_i = i - 1; itr_i >= 0 && j - (i - itr_i) >= 0; itr_i --)
-        diag = grid[itr_i][j - (i - itr_i)] * (diag % MOD + f(grid, dp, itr_i, j - (i - itr_i)) % MOD) % MOD;
+#define ll                      long long int
+#define ull                     unsigned ll
+#define ld                      long double
+#define p(x, y)                 pair<x, y> 
+#define pb                      push_back
+#define fi                      first
+#define se                      second
+#define lb                      lower_bound
+#define ub                      upper_bound
+#define vll                     vector<ll>
+#define ninf                    ((ll)((-1)*1e18+5))
+#define inf                     ((ll)(1e18+5))
+#define MOD                     ((ll)(1e9+7))
+#define nmin(v)                 *min_element(all(v))
+#define nmax(v)                 *max_element(all(v))
+#define rpt(i, begin, end)      for (__typeof(end) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+#define all(x)                  (x).begin(), (x).end() 
+#define yes                     cout << "YES";
+#define no                      cout << "NO";
+#define nl                      cout << "\n";
+#define kill(x)                 {cout << x << "\n"; return; }
+#define TESTCASE
+#define SIEVE_SIZE                ((ll)(1e5))
+/*_________________________________________________________________________________________________________________________________________*/
 
-    // up
-    long long left = 0;
-    for(int itr_j = j - 1; itr_j >= 0; itr_j--)
-        left  = grid[i][itr_j] * (left % MOD + f(grid, dp, i, itr_j) % MOD) % MOD;
+template<class T>
+T inline max(T a, T b, T c, T d = ninf, T e = ninf);
+template<class T>
+T inline min(T a, T b, T c, T d = inf, T e = inf);
+template<class T>
+void inline print(vector<T> v);
+template<class T>
+void inline print(vector<vector<T>> v);
 
-    // left
-    long long up = 0;
-    for(int itr_i = i - 1; itr_i >= 0; itr_i--) {
+bitset<SIEVE_SIZE> isComposit;
+void setSieve();
 
-        up  = grid[itr_i][j] * (up % MOD + f(grid, dp, itr_i, j) % MOD) % MOD;
-    }
+set<ll> allPrimes;
+void storePrimes();
+/*_________________________________________________________________________________________________________________________________________*/
+/*_________________________________________________________________________________________________________________________________________*/
 
-    return dp[i][j] = (up + left + diag);
+void init(){
+
+    return;
 }
+
 
 void solve(void){
-    
-    long long n, m; cin >> n >> m;
-    vector<vector<long long>> grid(n, vector<long long> (m, 0));
-    vector<vector<long long>> dp(n, vector<long long> (m, -1));
-    for(int i = 0; i < n; i++)
-        for(int j = 0; j < m; j++)
-            cin >> grid[i][j];
-    cout << f(grid, dp, n - 1, m - 1);
+    ll n; cin >> n;
+    ll x; cin >> x;
+
+    ll gcd = x, mmax = x; 
+    rpt(i, 0, n - 1) {
+        cin >> x;
+        gcd = __gcd(gcd, x);
+        mmax = max(mmax, x);
+    }
+
+    cout << mmax / gcd;
+
+    nl;
 }
 
+
+/*_________________________________________________________________________________________________________________________________________*/
+/*_________________________________________________________________________________________________________________________________________*/
+
 int main() {
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    solve();
+    Timer _;
+    srand(time(0));
+    #ifdef DarkLord
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+        freopen("err.txt","w",stderr);
+    #endif
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    
+    init();
+
+    ll t = 1;
+
+    #ifdef TESTCASE
+        cin >> t;
+    #endif
+
+    while(t--) 
+        solve();
+
     return 0;
 }
+
+/*_________________________________________________________________________________________________________________________________________*/
+
+template<class T>
+T inline max(T a, T b, T c, T d, T e){
+    return max(max(max(a, b), max(c, d)), e);
+}
+
+template<class T>
+T inline min(T a, T b, T c, T d, T e){
+    return min(min(min(a, b), min(c, d)), e);
+}
+
+template<class T>
+void inline print(vector<T> v){
+    for(auto x : v)
+        cout << x << " ";
+}
+
+template<class T>
+void inline print(vector<vector<T>> v){
+    for(auto x : v){
+        for(auto y : x)
+            cout << y << " ";
+        nl;
+    }
+}
+
+void setSieve(){
+    isComposit[0] = isComposit[1] = 1;
+    for(long long i = 2; i*i <= SIEVE_SIZE; i++)
+        if(isComposit[i] == 0)
+            for(long long j = i*i; j <= SIEVE_SIZE; j += i)
+                isComposit[j] = 1;
+
+}
+
+void storePrimes(){
+    if(isComposit[2])
+        setSieve();
+    for(ll i = 2; i < SIEVE_SIZE; i++)
+        if(!isComposit[i])
+            allPrimes.insert(i);
+}
+
+/*_________________________________________________________________________________________________________________________________________*/
