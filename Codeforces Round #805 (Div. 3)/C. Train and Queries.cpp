b6fd5959fa25bdd94b8e7a4922a069a86beed254
Harsh Upadhayay <harshupadhayay906@gmail.com>
2023-01-27 21:13:52 +0530

    data structures,greedy,*1100

--- Codeforces Round #805 (Div. 3)/C. Train and Queries.cpp
+++ Codeforces Round #805 (Div. 3)/C. Train and Queries.cpp
@@ -67,8 +67,8 @@ void solve(void){
 
     int x = -1;
     rpt(i, 1, 7) {
-        if(s / i < n) {
-            x = i - 1;
+        if(n <= (s / i)) {
+            x = i;
             break;
         }
     }
