5d19f2bf0720ce868ac1596fc5d7132b702207db
Harsh Upadhayay <harshupadhayay906@gmail.com>
2023-02-03 20:24:42 +0530

    data structures,greedy,*1100

--- Codeforces Round #805 (Div. 3)/C. Train and Queries.cpp
+++ Codeforces Round #805 (Div. 3)/C. Train and Queries.cpp
@@ -69,7 +69,8 @@ void solve(void){
     rpt(i, n, 0) rs.insert(s[i]), rf[i] = rs.size();
 
     ll ans = 0;
-    debug(lf, rf);
+    debug(lf);
+    debug( rf);
     rpt(i, 0, n) {
 
         ans = max(lf[i] + rf[i], ans);
