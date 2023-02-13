e2b6f7ae811a6f8810487ddf3540921953ed9ffe
Harsh Upadhayay <harshupadhayay906@gmail.com>
2023-02-13 16:12:49 +0530

    data structures,greedy,*1100

--- Codeforces Round #805 (Div. 3)/C. Train and Queries.cpp
+++ Codeforces Round #805 (Div. 3)/C. Train and Queries.cpp
@@ -63,8 +63,8 @@ void solve(void){
     
    
 
-    vector<int> arr = {1,2,3,4,5,6,7,8,9,10,11};
-    int n = 11;
+    vector<int> arr = {1,1,2,2,2,3,3,3,3};
+    int n = 9;
 
     set<int> uniqVals;
     int maxLen = 0,
