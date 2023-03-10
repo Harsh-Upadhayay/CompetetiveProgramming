9fcf474b2ee36362a8046551fec8ab5a3b9c16dd
Harsh Upadhayay <harshupadhayay906@gmail.com>
2023-03-10 14:41:22 +0530

    data structures,greedy,*1100

--- Codeforces Round 805 (Div. 3)/C. Train and Queries.cpp
+++ Codeforces Round 805 (Div. 3)/C. Train and Queries.cpp
@@ -83,7 +83,6 @@ void solve() {
 
     ll n; cin >> n; 
     vector<vector<ll>> grid(n, vll(n, 0));
-    vector<vector<ll>> dp(n, vll(n, -1));
 
     rpt(i, 0, n)
         rpt(j, 0, n) {
@@ -91,7 +90,25 @@ void solve() {
             grid[i][j] = ch == '.';
         }
 
-    cout << fun(grid, dp, 0, 0);
+    vector<vector<ll>> dp(n, vll(n, 0));
+    dp[n - 1][n - 1] = 1;
+
+    for(int i = n - 1; i >= 0; i--) {
+        for(int j = n - 1; j >= 0; j--) {
+
+            ll right = 0, down = 0;
+
+            if(!grid[i][j])
+                dp[i][j] = 0;
+
+            if(j + 1 < n) right = dp[i][j + 1];
+            if(i + 1 < n) down = dp[i + 1][j];
+
+            dp[i][j] = right + down;
+        }
+    }
+
+    cout << dp[0][0];
 
 }
 
