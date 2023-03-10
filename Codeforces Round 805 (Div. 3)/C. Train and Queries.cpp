cc850fd3388386599233f1a0311e01d468cb43c8
Harsh Upadhayay <harshupadhayay906@gmail.com>
2023-03-11 00:32:02 +0530

    data structures,greedy,*1100

--- Codeforces Round 805 (Div. 3)/C. Train and Queries.cpp
+++ Codeforces Round 805 (Div. 3)/C. Train and Queries.cpp
@@ -101,6 +101,7 @@ void solve(void){
 
             if(mp.find(x) != mp.end()) {
                 cout << mp[x].fi + 1 << " " << mp[x].se + 1<< " " << i + 1 << " " << j + 1 << "\n";
+                cout << x << "\n";
                 return ;
             }
             else
