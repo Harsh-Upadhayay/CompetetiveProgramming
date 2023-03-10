233c614d268f939e619ea7c395a7cf96349236cf
Harsh Upadhayay <harshupadhayay906@gmail.com>
2023-03-11 00:40:04 +0530

    data structures,greedy,*1100

--- Codeforces Round 805 (Div. 3)/C. Train and Queries.cpp
+++ Codeforces Round 805 (Div. 3)/C. Train and Queries.cpp
@@ -100,7 +100,7 @@ void solve(void){
         rpt(j, i + 1, n) {
 
             int x = pathWt[i] ^ pathWt[j];
-            debug(i, j, x);
+            debug(i + 1, j + 1, x);
             if(mp.find(x) != mp.end()) {
                 cout << mp[x].fi + 1 << " " << mp[x].se + 1 << " " << i + 1 << " " << j + 1 << "\n";
                 cout << x << "\n";
