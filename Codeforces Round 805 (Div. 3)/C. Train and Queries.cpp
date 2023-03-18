fa37ae5590a29d2aeddf16f5c33fda955c485071
Harsh Upadhayay <harshupadhayay906@gmail.com>
2023-03-18 19:38:16 +0530

    data structures,greedy,*1100

--- Codeforces Round 805 (Div. 3)/C. Train and Queries.cpp
+++ Codeforces Round 805 (Div. 3)/C. Train and Queries.cpp
@@ -64,6 +64,18 @@ void solve(void){
     ll n; cin >> n;
     vll v(2 * n); rpt(i, 0, 2*n) cin >> v[i];
 
+    ll maxE = nmax(v), maxIdx = -1;
+    rpt(i, 0, 2*n) if(v[i] == maxE) maxIdx = i;
+
+    ll tsum = 0, ans = 0;
+    for(ll x : v) tsum += x;
+
+    vll q(2 * n, -1); 
+    q[maxIdx] = n;
+
+    rpt(i, 0, 2*n) ans += abs(v[i] - q[i]);
+
+    cout << min(ans, tsum);
 
     debug(v);
     
