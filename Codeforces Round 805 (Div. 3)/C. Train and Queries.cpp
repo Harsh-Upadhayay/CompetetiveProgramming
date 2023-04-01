39498de665699b4bcd1b014097cbb212d229db2a
Harsh Upadhayay <harshupadhayay906@gmail.com>
2023-04-01 17:12:23 +0530

    data structures,greedy,*1100

--- Codeforces Round 805 (Div. 3)/C. Train and Queries.cpp
+++ Codeforces Round 805 (Div. 3)/C. Train and Queries.cpp
@@ -83,7 +83,7 @@ pair<ll, ll> fun(string &ls, string &rs, ll i, ll upTight, ll loTight) {
     ll ub = upTight ? rs[rs.size() - i] - '0' : 9,
         lb = loTight ? ls[ls.size() - i] - '0' : 0,
         ansSum = 0, ansFreq = 0;
-    debug(lb, ub, upTight, loTight);
+    debug(lb, ub, rs[rs.size()]);
     rpt(i, 0, 10) {
 
         if(i >= lb && i <= ub) {
