4e11f21c1a541b792eeeb14c49c28b3d37793cf1
Harsh Upadhayay <harshupadhayay906@gmail.com>
2023-04-01 19:35:36 +0530

    data structures,greedy,*1100

--- Codeforces Round 805 (Div. 3)/C. Train and Queries.cpp
+++ Codeforces Round 805 (Div. 3)/C. Train and Queries.cpp
@@ -70,7 +70,7 @@ pair<ll, pair<ll, ll>> fun(string &ls, string &us, ll n, bool utt, bool ltt) {
     rpt(i, 1, (ll)10) {
 
         if(i >= lb && i <= ub) {
-            debug(i);
+            debug(i, utt, ltt);
             auto it = fun(ls, us, n - 1, utt && (i == ub), ltt && (i == lb));
             ll pmax = it.second.first, pmin = it.second.second;
 
