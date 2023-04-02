2ce2a2e7dd2146b261216ded811334aa0aa92243
Harsh Upadhayay <harshupadhayay906@gmail.com>
2023-04-02 12:11:13 +0530

    data structures,greedy,*1100

--- Codeforces Round 805 (Div. 3)/C. Train and Queries.cpp
+++ Codeforces Round 805 (Div. 3)/C. Train and Queries.cpp
@@ -155,9 +155,7 @@ string optimized(ll l, ll r) {
     ll minDiff = fun(ls, rs, dp, n, 1, 1, 0, 9, 1);
     cout << minDiff;
 
-    string num = "*";
-
-    cout << num;
+    string num = "";
     num = conNum(ls, rs, num, n, minDiff, 1, 1, 0, 9);
     removeLeadz(num);
     return num;
