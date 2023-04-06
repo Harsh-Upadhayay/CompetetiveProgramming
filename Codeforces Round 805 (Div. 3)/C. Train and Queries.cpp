94fbb6e09c5eb160e8657076da56b6976d2f70ae
Harsh Upadhayay <harshupadhayay906@gmail.com>
2023-04-06 20:14:49 +0530

    data structures,greedy,*1100

--- Codeforces Round 805 (Div. 3)/C. Train and Queries.cpp
+++ Codeforces Round 805 (Div. 3)/C. Train and Queries.cpp
@@ -32,7 +32,7 @@ using namespace std;
 #define no                      cout << "NO";
 #define nl                      cout << "\n";
 #define kill(x)                 {cout << x << "\n"; return; }
-// #define TESTCASE
+#define TESTCASE
 #define SIEVE_SIZE                ((ll)(1e5))
 /*_________________________________________________________________________________________________________________________________________*/
 
@@ -65,57 +65,16 @@ void init(){
 }
 
 
-pair<ll, ll> fun(vector<vector<ll>> &grid, vector<vector<pair<ll, ll>>> &dp, ll i, ll x) {
-
-    ll n = grid.size(), m = grid[0].size();
-
-    if(i == n)
-        return {x > 0 ? 1 : 0, -1};
-
-    if(dp[i][x].first != -1)
-        return dp[i][x];
-
-    ll ans = -1,
-        idx = -1;
-
-    rpt(j, 0, m) {
-        auto it = fun(grid, dp, i + 1, x ^ grid[i][j]);
-
-        if(it.fi == 1) {
-            return dp[i][x] = {1, j};
-        }
-    }
-    return {0, -1};
-    // return dp[i][x] = {ans, idx};
-}
-
 void solve(void){
     
-    ll xMax = 1024;
+    ll n, k; cin >> n >> k;
 
-    ll n, m; cin >> n >> m;
-    vector<vector<ll>> grid(n, vector<ll> (m, 0));
-    rpt(i, 0, n)
-        rpt(j, 0, m)
-            cin >> grid[i][j];
+    if(n % k)
+        kill("NO");
 
-    vector<vector<pair<ll, ll>>> dp(n + 1, vector<pair<ll, ll>> (xMax, {-1, -1}));
+    ll z = n / k;
 
-    if(fun(grid, dp, 0, 0).fi == 0)
-        kill("NIE");
-
-    cout << "TAK\n";
-    debug(dp);
-    rpt(i, 0, n + 1) {
-        rpt(j, 0, xMax) {
-
-            if(dp[i][j].se != -1) {
-                cout << dp[i][j].se + 1 << " ";
-                break;
-            }
-
-        }
-    }
+    cout << "YES";
 
     nl;
 }
