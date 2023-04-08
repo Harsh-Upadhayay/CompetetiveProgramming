22fc71e838c7b5d6f2bc6a818c0c2dc9fb513d59
Harsh Upadhayay <harshupadhayay906@gmail.com>
2023-04-08 23:47:47 +0530

    data structures,greedy,*1100

--- Codeforces Round 805 (Div. 3)/C. Train and Queries.cpp
+++ Codeforces Round 805 (Div. 3)/C. Train and Queries.cpp
@@ -98,9 +98,8 @@ void solve(void){
             next[j][k] = inf;
     
 
-    // for(int i = 0; i <= n; i++)
-    //     for(int j = 0; j <= n; j++)
-    //         dp[i][j][0] = 0;
+        for(int j = 0; j <= n; j++)
+            next[j][0] = curr[j][0] = 0;
 
     for(int i = n - 1; i >= 0; i--) {
         for(int j = n - 2; j >= -1; j--) {
