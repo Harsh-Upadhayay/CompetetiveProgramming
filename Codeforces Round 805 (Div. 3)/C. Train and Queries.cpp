16df45bb1b7deaaebd2bd91e842eeb754c54b35d
Harsh Upadhayay <harshupadhayay906@gmail.com>
2023-04-10 23:05:59 +0530

    data structures,greedy,*1100

--- Codeforces Round 805 (Div. 3)/C. Train and Queries.cpp
+++ Codeforces Round 805 (Div. 3)/C. Train and Queries.cpp
@@ -86,7 +86,7 @@ void solve(void){
     ll n; cin >> n;
     vector<vector<ll>> v(2, vll(n)); cin >> v[0] >> v[1];
 
-    vector<vector<ll>> dp(n + 1, vll(2));
+    vector<vector<ll>> dp(n + 1, vll(2, -1));
     cout << fun(v, dp, n - 1, 0);
 
     nl;
