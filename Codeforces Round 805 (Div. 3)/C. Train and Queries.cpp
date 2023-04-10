ca408ee11dbaa5eb93742c5f5c28f622d564eb89
Harsh Upadhayay <harshupadhayay906@gmail.com>
2023-04-11 01:08:13 +0530

    data structures,greedy,*1100

--- Codeforces Round 805 (Div. 3)/C. Train and Queries.cpp
+++ Codeforces Round 805 (Div. 3)/C. Train and Queries.cpp
@@ -99,9 +99,9 @@ void solve(void){
 
     vector<vector<ll>> dp(maxD + 4, vll (maxD + 1, 0));
     
+    for(int i = maxD; i >= d; i--) {
 
-    for(int j = 1; j <= maxD; j++) {    
-        for(int i = maxD; i >= d; i--) {
+        for(int j = 1; j <= maxD; j++) {
 
                 ll r = 0, m = 0, l = 0;
                 if(i + j + 1 <= maxD)
@@ -113,8 +113,8 @@ void solve(void){
 
                 dp[i][j] = dim[i] + max({r, m, l});
 
-
         }
+
     }
 
     cout << dp[d][d];
