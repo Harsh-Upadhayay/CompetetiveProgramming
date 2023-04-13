e286a3b253c020a580205c38a879bc2baae833e3
Harsh Upadhayay <harshupadhayay906@gmail.com>
2023-04-13 16:34:37 +0530

    data structures,greedy,*1100

--- Codeforces Round 805 (Div. 3)/C. Train and Queries.cpp
+++ Codeforces Round 805 (Div. 3)/C. Train and Queries.cpp
@@ -1,13 +1,16 @@
 /*  */
 #include<bits/stdc++.h>
 using namespace std;
+
 #ifdef DarkLord
     #include "debug.h"
 #else
     #define debug(x...) 
     class Timer{};
 #endif
+
 /*_________________________________________________________________________________________________________________________________________*/
+
 #define ll                      long long int
 #define ull                     unsigned ll
 #define ld                      long double
@@ -32,11 +35,13 @@ using namespace std;
 // #define TESTCASE
 #define SIEVE_SIZE                ((ll)(1e5))
 /*_________________________________________________________________________________________________________________________________________*/
+
 template <typename T>
 istream& operator>>(istream &is, vector<T> &v) {
     rpt(i, 0, v.size()) is >> v[i];
     return is;
 }
+
 template<class T>
 T inline max(T a, T b, T c, T d = ninf, T e = ninf);
 template<class T>
@@ -45,64 +50,82 @@ template<class T>
 void inline print(vector<T> v);
 template<class T>
 void inline print(vector<vector<T>> v);
+
 bitset<SIEVE_SIZE> isComposit;
 void setSieve();
+
 set<ll> allPrimes;
 void storePrimes();
 /*_________________________________________________________________________________________________________________________________________*/
 /*_________________________________________________________________________________________________________________________________________*/
+
 void init(){
+
     return;
 }
-ll maxD = 3000;
-ll fun(map<ll, ll> &dim,  vector<vector<ll>> &dp, ll i, ll j) {
-    
-    if(j > maxD || i == 0) {
-        return 0;
-    }
-    if(dp[i][j] != -1)
-        return dp[i][j];
-    return dp[i][j] = dim[j] + max({fun(dim, dp, i + 1, j + i + 1), fun(dim, dp, i, j + i), fun(dim, dp, i - 1, j + i - 1)});
+
+bool fun(vll &v, ll m, vector<vector<vector<ll>>> &dp, ll i, ll j, ll k) {
+
+    if(i < 0)
+        return k && !(j % m);
+
+    if(dp[i][j][k] != -1)
+        return dp[i][j][k];
+
+    if(k & !(j % m))
+        return 1;
+
+    ll take = 0, nottake = 0;
+
+    take = fun(v, m, dp, i - 1, (j + v[i]) % m, 1);
+    nottake = fun(v, m, dp, i - 1, j, k);
+
+    return dp[i][j][k] = take | nottake;
 }
+
 void solve(void){
     
-    ll n, d; cin >> n >> d;
+    ll n, m; cin >> n >> m;
     vll v(n); cin >> v;
-    map<ll, ll> dim;
-    for(ll x : v) dim[x]++;
 
-    vector<vector<ll>> dp(maxD + 4, vll (maxD + 1, 0));
+    debug(n, m, v);
 
-    for(int i = maxD; i >= d; i--) {
-        for(int j = 1; j <= maxD; j++) { 
-            ll r = 0, m = 0, l = 0;
-            if(i + j + 1 <= maxD)
-                r = dp[i + j + 1][j + 1];
-            if(i + j <= maxD)
-                m = dp[i + j][j];
-            if(j != 1 && i + j - 1 <= maxD)
-                l = dp[i + j - 1][j - 1];
-            dp[i][j] = dim[i] + max({r, m, l});
+    vector<vector<vector<ll>>> dp(n + 1, vector<vector<ll>> (m, vector<ll>(2, 0)));
+
+    // vector<vector<ll>> prv(m, vector<ll>(2, 0)), cur(prv);
+
+    rpt(j, 0, m) 
+        dp[0][j][1] = !(j % m);
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            for(int k = 1; k >= 0; k--) {
+
+                ll take = 0, nottake = 0;
+
+                take = dp[i][(j + v[i]) % m][1];
+                nottake = dp[i][j][k];
+
+                dp[i + 1][j][k] = take | nottake;               
+
+            }
         }
     }
-    // debug(dp);
-    // for(int i = 1; i <= maxD; i++) {
-    //     for(int j = maxD; j >= d; j--) {
-    //         ll r = 0, m = 0, l = 0;
-    //         if(i + j + 1 <= maxD)
-    //             r = dp[i + 1][i + j + 1];
-    //         if(i + j <= maxD)
-    //             m = dp[i][i + j];
-    //         if(i != 1 && i + j - 1 <= maxD)
-    //             l = dp[i - 1][i + j - 1];
-    //         dp[i][j] = dim[j] + max({r, m, l});
-    //     }
-    // }
-    cout << dp[d][d];
+
+    bool ans = dp[n][0][0];
+
+    if(ans)
+        cout << "YES";
+    else
+        cout <<"NO";
+
     nl;
 }
+
+
 /*_________________________________________________________________________________________________________________________________________*/
 /*_________________________________________________________________________________________________________________________________________*/
+
 int main() {
     Timer _;
     srand(time(0));
@@ -111,32 +134,42 @@ int main() {
         freopen("output.txt","w",stdout);
         freopen("err.txt","w",stderr);
     #endif
+
     ios_base::sync_with_stdio(0);
     cin.tie(NULL);
     
     init();
+
     ll t = 1;
+
     #ifdef TESTCASE
         cin >> t;
     #endif
+
     while(t--) 
         solve();
+
     return 0;
 }
+
 /*_________________________________________________________________________________________________________________________________________*/
+
 template<class T>
 T inline max(T a, T b, T c, T d, T e){
     return max(max(max(a, b), max(c, d)), e);
 }
+
 template<class T>
 T inline min(T a, T b, T c, T d, T e){
     return min(min(min(a, b), min(c, d)), e);
 }
+
 template<class T>
 void inline print(vector<T> v){
     for(auto x : v)
         cout << x << " ";
 }
+
 template<class T>
 void inline print(vector<vector<T>> v){
     for(auto x : v){
@@ -145,13 +178,16 @@ void inline print(vector<vector<T>> v){
         nl;
     }
 }
+
 void setSieve(){
     isComposit[0] = isComposit[1] = 1;
     for(long long i = 2; i*i <= SIEVE_SIZE; i++)
         if(isComposit[i] == 0)
             for(long long j = i*i; j <= SIEVE_SIZE; j += i)
                 isComposit[j] = 1;
+
 }
+
 void storePrimes(){
     if(isComposit[2])
         setSieve();
@@ -159,3 +195,5 @@ void storePrimes(){
         if(!isComposit[i])
             allPrimes.insert(i);
 }
+
+/*_________________________________________________________________________________________________________________________________________*/
