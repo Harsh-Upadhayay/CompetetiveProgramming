86593729611aa0c5ea52601ca7e1a10883f40ccf
Harsh Upadhayay <harshupadhayay906@gmail.com>
2023-04-24 20:48:06 +0530

    data structures,greedy,*1100

--- Codeforces Round 805 (Div. 3)/C. Train and Queries.cpp
+++ Codeforces Round 805 (Div. 3)/C. Train and Queries.cpp
@@ -94,6 +94,9 @@ void solve(ll __T__){
     rpt(i, 0, n / 2) 
         if(s[i] == s[n - i - 1])
             mp[i] = s[i];
+
+    ll ans = ceil(mp.size() / 2.0);
+    cout << ans;
     debug(mp);
 
     nl;
