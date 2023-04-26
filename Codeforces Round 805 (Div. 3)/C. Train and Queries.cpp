bc9e15647a8e56755513cca441c30ccbc868f692
Harsh Upadhayay <harshupadhayay906@gmail.com>
2023-04-26 20:17:37 +0530

    data structures,greedy,*1100

--- Codeforces Round 805 (Div. 3)/C. Train and Queries.cpp
+++ Codeforces Round 805 (Div. 3)/C. Train and Queries.cpp
@@ -95,8 +95,12 @@ void solve(ll __T__){
         b -= 1;
 
     c = x - (a * b);
-   
-    cout << a << " " << b << " " << c;
+    
+    if(a <= 0 || b <= 0 || c <= 0 ||
+        a > 1e6 || b >= 1e6 || c >= 1e6)
+        cout << -1;
+    else
+        cout << a << " " << b << " " << c;
     nl;
 }
 
