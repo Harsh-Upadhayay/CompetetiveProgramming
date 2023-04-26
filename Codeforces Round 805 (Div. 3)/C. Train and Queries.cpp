3491c9a0b316b222da4b3441b30ee31fd2cf212e
Harsh Upadhayay <harshupadhayay906@gmail.com>
2023-04-26 12:14:29 +0530

    data structures,greedy,*1100

--- Codeforces Round 805 (Div. 3)/C. Train and Queries.cpp
+++ Codeforces Round 805 (Div. 3)/C. Train and Queries.cpp
@@ -96,7 +96,7 @@ vector<nd> segT;
 
 
 void build(string &s, ll ind, ll low, ll high) {
-
+    debug(ind, low, high);
     if(low == high) {
         segT[ind] = nd(s[low] == '(', 0, s[high] == ')');
         return;
@@ -111,8 +111,10 @@ void build(string &s, ll ind, ll low, ll high) {
     segT[ind].p = segT[2 * ind + 1].p + segT[2 * ind + 2].p;
     segT[ind].c = segT[2 * ind + 1].c + segT[2 * ind + 2].c;
     
-    segT[ind].p += max(0ll, min(segT[2 * ind + 1].o - segT[2 * ind + 1].p, 
-                        segT[2 * ind + 2].o - segT[2 * ind + 2].p));
+    segT[ind].p += max(0ll, min(
+                        segT[2 * ind + 1].o - segT[2 * ind + 1].p, 
+                        segT[2 * ind + 2].o - segT[2 * ind + 2].p
+                        ));
 
 }
 
